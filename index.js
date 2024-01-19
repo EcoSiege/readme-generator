f000b2f82f68ef4cc3e16e8e2baca8e0b6b55d11
EcoSiege <153530802+EcoSiege@users.noreply.github.com>
2024-01-19 15:53:07 -0600

    started inquirer prompt.

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'Enter your project title.',
+            name: 'title'
+        }, {
+            type: 'input',
+            message: 'Enter your project description.',
+            name: 'description'
+        }, {
+            type: 'input',
+            message: 'Enter usage information about your project.',
+            name: 'usage'
+        }, {
+            type: 'input',
+            message: 'Enter your projects contruibtors',
+            name: 'contruibtors'  
+        }, {
+            type: 'input',
+            message: 'Enter your projects test instructions',
+            name: 'test'  
+        }
+    ])
+
+const createREADME = `
+
+` 
